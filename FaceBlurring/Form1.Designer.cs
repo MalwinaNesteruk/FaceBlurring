4979b6d15d4cf176274376d01987704083219b80
MalwinaNesteruk <malwina.nesteruk@gmail.com>
2023-11-14 15:57:17 +0100

    Init commit.

---
+++ FaceBlurring/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace FaceBlurring
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pojedynczyObrazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.załadujObrazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wymażTwarzeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszObrazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderZObrazamiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.załadujObrazyZFolderuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wymażTwarzeNaPojedynczymObrazieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wymażTwarzeWeWszystkichObrazachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszPojedynczyObrazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszWszystkieObrazyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pojedynczyObrazToolStripMenuItem,
+            this.folderZObrazamiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pojedynczyObrazToolStripMenuItem
+            // 
+            this.pojedynczyObrazToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.załadujObrazToolStripMenuItem,
+            this.wymażTwarzeToolStripMenuItem,
+            this.zapiszObrazToolStripMenuItem});
+            this.pojedynczyObrazToolStripMenuItem.Name = "pojedynczyObrazToolStripMenuItem";
+            this.pojedynczyObrazToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.pojedynczyObrazToolStripMenuItem.Text = "Pojedynczy obraz";
+            // 
+            // załadujObrazToolStripMenuItem
+            // 
+            this.załadujObrazToolStripMenuItem.Name = "załadujObrazToolStripMenuItem";
+            this.załadujObrazToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.załadujObrazToolStripMenuItem.Text = "Załaduj obraz";
+            // 
+            // wymażTwarzeToolStripMenuItem
+            // 
+            this.wymażTwarzeToolStripMenuItem.Name = "wymażTwarzeToolStripMenuItem";
+            this.wymażTwarzeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.wymażTwarzeToolStripMenuItem.Text = "Wymaż twarze";
+            // 
+            // zapiszObrazToolStripMenuItem
+            // 
+            this.zapiszObrazToolStripMenuItem.Name = "zapiszObrazToolStripMenuItem";
+            this.zapiszObrazToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zapiszObrazToolStripMenuItem.Text = "Zapisz obraz";
+            // 
+            // folderZObrazamiToolStripMenuItem
+            // 
+            this.folderZObrazamiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.załadujObrazyZFolderuToolStripMenuItem,
+            this.wymażTwarzeNaPojedynczymObrazieToolStripMenuItem,
+            this.wymażTwarzeWeWszystkichObrazachToolStripMenuItem,
+            this.zapiszPojedynczyObrazToolStripMenuItem,
+            this.zapiszWszystkieObrazyToolStripMenuItem});
+            this.folderZObrazamiToolStripMenuItem.Name = "folderZObrazamiToolStripMenuItem";
+            this.folderZObrazamiToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.folderZObrazamiToolStripMenuItem.Text = "Folder z obrazami";
+            // 
+            // załadujObrazyZFolderuToolStripMenuItem
+            // 
+            this.załadujObrazyZFolderuToolStripMenuItem.Name = "załadujObrazyZFolderuToolStripMenuItem";
+            this.załadujObrazyZFolderuToolStripMenuItem.Size = new System.Drawing.Size(281, 22);
+            this.załadujObrazyZFolderuToolStripMenuItem.Text = "Załaduj obrazy z folderu";
+            // 
+            // wymażTwarzeNaPojedynczymObrazieToolStripMenuItem
+            // 
+            this.wymażTwarzeNaPojedynczymObrazieToolStripMenuItem.Name = "wymażTwarzeNaPojedynczymObrazieToolStripMenuItem";
+            this.wymażTwarzeNaPojedynczymObrazieToolStripMenuItem.Size = new System.Drawing.Size(281, 22);
+            this.wymażTwarzeNaPojedynczymObrazieToolStripMenuItem.Text = "Wymaż twarze na pojedynczym obrazie";
+            // 
+            // wymażTwarzeWeWszystkichObrazachToolStripMenuItem
+            // 
+            this.wymażTwarzeWeWszystkichObrazachToolStripMenuItem.Name = "wymażTwarzeWeWszystkichObrazachToolStripMenuItem";
+            this.wymażTwarzeWeWszystkichObrazachToolStripMenuItem.Size = new System.Drawing.Size(281, 22);
+            this.wymażTwarzeWeWszystkichObrazachToolStripMenuItem.Text = "Wymaż twarze we wszystkich obrazach";
+            // 
+            // zapiszPojedynczyObrazToolStripMenuItem
+            // 
+            this.zapiszPojedynczyObrazToolStripMenuItem.Name = "zapiszPojedynczyObrazToolStripMenuItem";
+            this.zapiszPojedynczyObrazToolStripMenuItem.Size = new System.Drawing.Size(281, 22);
+            this.zapiszPojedynczyObrazToolStripMenuItem.Text = "Zapisz pojedynczy obraz";
+            // 
+            // zapiszWszystkieObrazyToolStripMenuItem
+            // 
+            this.zapiszWszystkieObrazyToolStripMenuItem.Name = "zapiszWszystkieObrazyToolStripMenuItem";
+            this.zapiszWszystkieObrazyToolStripMenuItem.Size = new System.Drawing.Size(281, 22);
+            this.zapiszWszystkieObrazyToolStripMenuItem.Text = "Zapisz wszystkie obrazy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Face Blurring";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem pojedynczyObrazToolStripMenuItem;
+        private ToolStripMenuItem załadujObrazToolStripMenuItem;
+        private ToolStripMenuItem wymażTwarzeToolStripMenuItem;
+        private ToolStripMenuItem zapiszObrazToolStripMenuItem;
+        private ToolStripMenuItem folderZObrazamiToolStripMenuItem;
+        private ToolStripMenuItem załadujObrazyZFolderuToolStripMenuItem;
+        private ToolStripMenuItem wymażTwarzeNaPojedynczymObrazieToolStripMenuItem;
+        private ToolStripMenuItem wymażTwarzeWeWszystkichObrazachToolStripMenuItem;
+        private ToolStripMenuItem zapiszPojedynczyObrazToolStripMenuItem;
+        private ToolStripMenuItem zapiszWszystkieObrazyToolStripMenuItem;
+    }
+}
